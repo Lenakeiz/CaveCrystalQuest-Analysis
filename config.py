7973ee3a28a3d57cd6d8d2edf4d0a87c70fa8307
Andrea <andrea.castegnaro@outlook.com>
2023-12-11 19:14:15 +0000

    Modified plot for automatically creating figures for each participant.

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 # config.py
 folder_names = ["yc", "mc", "ec", "mci_plus", "mci_minus"]
+color_palette = ["#ab2346","#2d82b7","#2b2d42","#f2c57c","#eb8a90"]
--- plot_trial_route.py
+++ plot_trial_route.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Arc, RegularPolygon
 import math
 import os
+import re
+
+from config import folder_names
+from config import color_palette
 
 def normalise_angle(angle):
     if angle > 360:
@@ -13,16 +17,16 @@ def normalise_angle(angle):
     else:
         return angle
    
-def drawCircArrow(ax,radius,centX,centY,angle_,theta2_,orientation_,arrowDirection_,color_):
+def drawCircArrow(ax,radius,centX,centY,angle_,theta2_,orientation_,arrowDirection_,color_,linewidth = 1.2):
     
     #Create the line   
-    arc = Arc([centX,centY],radius,radius,angle=angle_,theta1=0,theta2=theta2_,capstyle='round',linestyle=':',lw=1.2,color=color_)
+    arc = Arc([centX,centY],radius,radius,angle=angle_,theta1=0,theta2=theta2_,capstyle='round',linestyle=':',lw=linewidth,color=color_)
     ax.add_patch(arc)
     
     #Create triangle as arrow head
     arrowHead_X=centX+(radius/2)*np.cos(math.radians(arrowDirection_)) #Do trig to determine crystalOrigin_ position
     arrowHead_Y=centY+(radius/2)*np.sin(math.radians(arrowDirection_))  
-    arrowHead = RegularPolygon((arrowHead_X, arrowHead_Y),numVertices=3,radius=radius/20,orientation=orientation_,color=color_)
+    arrowHead = RegularPolygon((arrowHead_X, arrowHead_Y),numVertices=3,radius=radius/20,orientation=orientation_,color=color_,linewidth=linewidth)
     ax.add_patch(arrowHead)
     
     # Make sure you keep the axes scaled or else arrow will distort
@@ -30,20 +34,40 @@ def drawCircArrow(ax,radius,centX,centY,angle_,theta2_,orientation_,arrowDirecti
     
 def visualise_ccq_trial(d,trial_num):
     
+    #plotting variables
+    head_width = 0.1
+    line_width= 2
+    starting_marker_size = 12
+    crystal_marker_size_spawn = 9
+    crystal_marker_size_reposition = 16
+    starting_position_color = color_palette[4]
+    arrow_encoding_distance_color = color_palette[4]
+    fog_position_color = color_palette[0]
+    encoding_angle_color = color_palette[1]
+    production_angle_color = color_palette[2]
+    circular_arrow_width = 3.0
+    text_font_size = 10
+    text_font_size_title = 10
+
+
     plt.figure(figsize=(10,10))
     # a: plot starting corner
-    plt.plot(d['startingCorner_x'],d['startingCorner_z'],marker="o",markersize=12, color=[244/255,232/255,108/255])
-
-    # b: plot walking path
-    dx = (d['turningEncodingPosition_x'] - d['startingCorner_x']) *0.95
-    dz = (d['turningEncodingPosition_z'] - d['startingCorner_z']) *0.95
-    plt.arrow(d['startingCorner_x'], d['startingCorner_z'], dx, dz,head_width = 0.2,lw=2,color=[244/255,232/255,108/255],length_includes_head=True)
+    plt.plot(d['startingCorner_x'],d['startingCorner_z'],marker="o",markersize=starting_marker_size, color=arrow_encoding_distance_color)    
     
-    # c: plot standing position in the fog
-    plt.plot(d['turningEncodingPosition_x'],d['turningEncodingPosition_z'],marker="o",markersize=12, color='green')
-
-    # d: plot the walking path extending beyond standing point 
-    plt.arrow(d['turningEncodingPosition_x'],d['turningEncodingPosition_z'],d['encodingDirection_x'],d['encodingDirection_z'],head_length=None,linestyle=':', color='green')
+    # b: plot walking path
+    dx = (d['turningEncodingPosition_x'] - d['startingCorner_x']) *1.0
+    dz = (d['turningEncodingPosition_z'] - d['startingCorner_z']) *1.0
+    plt.arrow(d['startingCorner_x'], d['startingCorner_z'], dx, dz,head_width = head_width,lw=line_width,color=arrow_encoding_distance_color,length_includes_head=True)
+    
+    # b: plot standing position in the fog
+    plt.plot(d['turningEncodingPosition_x'],d['turningEncodingPosition_z'],marker="o",markersize=starting_marker_size, color=fog_position_color)
+   
+    # d: plot the walking path extending beyond standing point
+    start_x = d['turningEncodingPosition_x']
+    start_z = d['turningEncodingPosition_z']
+    end_x = start_x + d['encodingDirection_x']
+    end_z = start_z + d['encodingDirection_z']
+    plt.plot([start_x, end_x], [start_z, end_z], lw=line_width, linestyle='--', color=arrow_encoding_distance_color)
     
     # e: plot where crystal is found
     encodingAngle = d['encodingAngle']                  
@@ -53,17 +77,18 @@ def visualise_ccq_trial(d,trial_num):
     crystalDirection = originalAngle-math.radians(encodingAngle)
     crystalOrigin_x = d['turningEncodingPosition_x'] + math.cos(crystalDirection)
     crystalOrigin_z = d['turningEncodingPosition_z'] + math.sin(crystalDirection)
-    plt.plot(crystalOrigin_x,crystalOrigin_z,marker="d",markersize=9,color="blue",alpha=0.7)
+    plt.plot(crystalOrigin_x,crystalOrigin_z,marker="d",markersize=crystal_marker_size_spawn,color="blue",alpha=1.0)
 
     # f: plot the direction facing where the crystal is found
-    plt.plot([d['turningEncodingPosition_x'], crystalOrigin_x], [d['turningEncodingPosition_z'], crystalOrigin_z], linestyle=':', color='blue')
-    
-    # g: plot homing direction
-    plt.arrow(d['turningEncodingPosition_x'], d['turningEncodingPosition_z'], d['productionDirection_x'], d['productionDirection_z'],head_width = 0.2,lw=2,color='purple')
+    plt.plot([d['turningEncodingPosition_x'], crystalOrigin_x], [d['turningEncodingPosition_z'], crystalOrigin_z], linestyle=':', color=encoding_angle_color)
     
     # h: plot where cystal is placed
-    plt.plot(d['productionDistance_x'],d['productionDistance_z'],marker="d",markersize=16,color='purple')
-    
+    plt.plot(d['productionDistance_x'],d['productionDistance_z'],marker="d",markersize=crystal_marker_size_reposition,color=production_angle_color)
+
+    # g: plot homing direction
+    plt.arrow(d['turningEncodingPosition_x'], d['turningEncodingPosition_z'], d['productionDirection_x'], d['productionDirection_z'],head_width = head_width,lw=line_width,color=production_angle_color, length_includes_head=True)
+        
+    ax = plt.gca()
     # i: plot encoding angle
     theta2_ = d['encodingAngle']
     if d['isEncodingClockwise']:
@@ -74,8 +99,7 @@ def visualise_ccq_trial(d,trial_num):
         angle_ = math.degrees(originalAngle)
         orientation_ = math.radians(angle_+theta2_)
         arrowDirection_ = theta2_+angle_
-    ax = plt.gca()
-    drawCircArrow(ax,1,d['turningEncodingPosition_x'],d['turningEncodingPosition_z'],angle_,theta2_,orientation_,arrowDirection_,color_='green')
+    drawCircArrow(ax,1,d['turningEncodingPosition_x'],d['turningEncodingPosition_z'],angle_,theta2_,orientation_,arrowDirection_,color_='green', linewidth=circular_arrow_width)
     
     # j: plot production angle    
     productionDirection = math.degrees(math.atan2(d['productionDirection_z'],d['productionDirection_x']))
@@ -91,13 +115,15 @@ def visualise_ccq_trial(d,trial_num):
         arrowDirection_ = theta2_+angle_         
     ap = plt.gca()
     drawCircArrow(ap,1.5,d['turningEncodingPosition_x'],d['turningEncodingPosition_z'],angle_,theta2_,orientation_,arrowDirection_,color_='blue')
-     
+    
+    # Set the limits for x-axis and y-axis
+    plt.xlim(-2.5, 2.5)
+    plt.ylim(-2.5, 2.5)
     plt.xlabel('X Position', fontsize=10)
     plt.ylabel('Z Position', fontsize=10)
     plt.axis('equal')
-    plt.title(f'Trial {trial_num} Route')
-    plt.savefig(f'../trial_graph/subj{subjid}_trial{trial_num}.png')
-    plt.close()
+    plt.title(f'Trial {trial_num} Route', fontsize=10)
+    
     return theta2_
 
 def calculate_error(d,theta2_):
@@ -125,18 +151,76 @@ def calculate_error(d,theta2_):
     return trial_error
     # print(f"Trial the encoding_angle is {encoding_angle}, the homing_angle is {homing_angle}, the production_angle is {production_angle}, the angular error is {angular_error:3f}. the encoding distance is {encoding_distance:3f}, the production distance is {production_distance:3f}, the linear error is {linear_error:3f}")
 
-if __name__ == "__main__":
-    subjid = input("Enter the participant ID: ")
-    data = pd.read_csv(os.path.abspath(f"data/{subjid}.csv"))
-    error_stats = pd.DataFrame(columns=['encoding_angle', 'homing_angle', 'production_angle', 'angular_error', 'encoding_distance','production_distance', 'linear_error'])
+def save_trial_figure(fig, trial_num, output_dir):
+    """
+    Saves the given figure to the specified output directory with a filename based on the trial number.
 
-    for trial_num in data['sequenceNumber']:
-        single_trial = data.iloc[[trial_num-1]].squeeze().to_dict()   
-        theta2_ = visualise_ccq_trial(single_trial,trial_num)
-        trial_error = calculate_error(single_trial,theta2_)
-        new_trial_error = pd.DataFrame([trial_error], columns=error_stats.columns)
-        error_stats = pd.concat([error_stats, new_trial_error], ignore_index=True)
-        error_stats.to_csv(f'../error_stats/error{subjid}.csv')
+    Parameters:
+    fig (matplotlib.figure.Figure): The figure to save.
+    trial_num (int): The trial number.
+    output_dir (str): The directory where the figure should be saved.
+    """
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    fig.savefig(os.path.join(output_dir, f'trial_{trial_num}.png'))
+    plt.close(fig)
+
+if __name__ == "__main__":
+
+    # Make sure that you have already the folder data and output created at the same level as this script
+    # Getting the directory where this script is located
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # create output folder
+    base_output_dir = os.path.join(script_dir,"output/trial_visualization")
+    base_input_dir = os.path.join(script_dir,"data")
+    if not os.path.exists(base_output_dir):
+        os.makedirs(base_output_dir)
+    
+    for folder_name in folder_names:
+        # create an input folder path specific for this category of people
+        input_folder_path = os.path.join(base_input_dir, folder_name)
+        # create an output folder specific for this category of people
+        output_dir = os.path.join(base_output_dir, folder_name)
+
+        if not os.path.exists(input_folder_path):
+            print(f"Input folder {input_folder_path} does not exist. Skipping...")
+            continue
+        
+        print(f"Processing folder {input_folder_path}...")
+
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        # processing files in the directory:
+        for file in os.listdir(input_folder_path):
+            if re.match(r"^\d+\.csv$", file):
+                subjid = file.split('.')[0]
+                subjid = re.match(r"^\d+", file).group(0)
+                subj_output_dir = os.path.join(output_dir, subjid)
+                
+                # creating a folder output for the participant
+                if not os.path.exists(subj_output_dir):
+                    os.makedirs(subj_output_dir)
+
+                data_path = os.path.join(input_folder_path, file)
+                data = pd.read_csv(os.path.join(data_path))
+                error_stats = pd.DataFrame(columns=['encoding_angle', 'homing_angle', 'production_angle', 'angular_error', 'encoding_distance','production_distance', 'linear_error'])
+
+                for trial_num in data['sequenceNumber']:
+
+                    print(f"Processing trial {trial_num} for participant {subjid}...")
+
+                    single_trial = data.iloc[[trial_num-1]].squeeze().to_dict()   
+                    theta2_ = visualise_ccq_trial(single_trial,trial_num)
+                    #trial_error = calculate_error(single_trial,theta2_)
+                    #new_trial_error = pd.DataFrame([trial_error], columns=error_stats.columns)
+                    #error_stats = pd.concat([error_stats, new_trial_error], ignore_index=True)
+                    #error_stats.to_csv(f'../error_stats/error{subjid}.csv')
+
+                    # Get the current figure and save it
+                    fig = plt.gcf()
+                    save_trial_figure(fig, trial_num, subj_output_dir)
     
     
     
